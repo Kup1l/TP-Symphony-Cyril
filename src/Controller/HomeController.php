ea5c4a367a92df478764edd6fabb293e3b719273
Kupil <cyrildumoulin59@yahoo.fr>
2021-04-23 08:58:30 +0200

    TP Twig
    fin du TP Twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Controller;
+use App\Entity\Article;
+use \DateTime;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,8 +15,24 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
+        $article1 = new Article();
+        $article1->setTitle('Title 1');
+        $article1->setSubtitle('SubTitle 1');
+        $article1->setCreatedAt(new DateTime());
+        $article1->setAuthor('John Doe');
+        $article1->setBody('Lorem ipsum dolor sit amet');
+        $article1->setImage('img/post-bg.jpg');
+
+        $article2 = new Article();
+        $article2->setTitle('Title 2');
+        $article2->setSubtitle('SubTitle 2');
+        $article2->setCreatedAt(new DateTime());
+        $article2->setAuthor('John Doe');
+        $article2->setBody('Lorem ipsum dolor sit amet');
+        $article2->setImage('img/post-bg.jpg');
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'articles' => [$article1, $article2]
         ]);
     }
 }
